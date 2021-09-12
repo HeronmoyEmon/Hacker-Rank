be8e9889277f18b31f7109ab79bd3430629b8775
Heronmoy Tanchangya Emon <89600307+HeronmoyEmon@users.noreply.github.com>
2021-09-12 19:21:51 +0600

    Add files via upload

---
+++ Multiplication Table using Loop.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+for i in range(1,11):
+    output = n*i
+    print(f"{n} x {i} = {output}")
+
+
+
+
+
+
