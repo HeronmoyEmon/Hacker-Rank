869f4b5275a188485fec726aedabd3a410e3f5d1
Heronmoy Tanchangya Emon <89600307+HeronmoyEmon@users.noreply.github.com>
2021-09-12 19:49:24 +0600

    Add files via upload

---
+++ print the even and odd indexes of a string by splitting it.py
@@ -0,0 +1,12 @@
+# for i in range(int(input())):
+#     s = input()
+#     print(s[::2], s[1::2])
+
+
+test_case_count = int(input())
+i = 0
+
+while i < test_case_count:
+    s = input('')
+    print(s[0::2], s[1::2])
+    i += 1 
